05821d7c46a6d3c397ff9066093020bfdcace8ab
lirantul123 <93045864+lirantul123@users.noreply.github.com>
2023-07-15 15:51:10 +0300

    Update check_login.php
    e

--- check_login.php
+++ check_login.php
@@ -1,13 +1,1 @@
-<?php
-session_start();
 
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if ($username === "AbCdEfGhI" && $password === "e") {
-    $_SESSION['authenticated'] = true;
-    header("Location: encryption.html");
-} else {
-    header("Location: login.html");
-}
-?>
